b30b90a9ed93cf8e98386bbf8d59ad40fc35c3c6
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-04-05 21:46:39 +0900

    update useInput

---
+++ src/useInput.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./styles.css";
+
+export const useInput = (initialState, validator) => {
+  const [value, setValue] = useState(initialState);
+
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+      console.log(willUpdate);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+
+  return { value, onChange };
+};
